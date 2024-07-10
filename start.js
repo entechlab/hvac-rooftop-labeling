159a68099d172ef7d95c5dc99d88e797af44f13a
Amir <Kimiyaie>
2024-07-10 11:01:05 -0400

    Website created v1.0

---
+++ start.js
@@ -0,0 +1,28 @@
+var examples = ["Cooling Tower(new)_leftImg8bit.png", "Cooling Tower-1_leftImg8bit.png", "Cooling Tower-2_leftImg8bit.png", "new_leftImg8bit.png", "new2_leftImg8bit.png"]
+const prefix_url = "https://nyinstituteoftechnology-my.sharepoint.com/:i:/r/personal/akimiyai_nyit_edu/Documents/imgs+masks/"
+
+function add_examples()
+{
+	for (var i=0;i<21;i++)
+	{
+		examples.push("example" + i + "_leftImg8bit.png")
+	}
+}
+
+function add_images()
+{
+	const div_imgs = document.getElementById("image_collection")
+	for (const path of examples)
+	{
+		var new_img = document.createElement("img")
+		new_img.src = prefix_url + path + "?csf=1&web"
+		new_img.className = "object-fill"
+		div_imgs.appendChild(new_img)
+		const br = document.createElement("br")
+		div_imgs.appendChild(br)
+	}
+}
+
+add_examples()
+add_images()
+
